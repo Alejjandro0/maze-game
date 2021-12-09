eb4a5aba2c127c2545f5b9d1e5441a972b518359
АлександрКондратюк <ankondratyuk_1@edu.hse.ru>
2021-12-09 15:48:37 +0300

    newtest

--- tests.py
+++ tests.py
@@ -1,10 +1,18 @@
 from main import arr, saveGame, loadGame, k
 
-def test_dada():
+def test_saveload():
     old_arr = arr.copy()
     saveGame(k)
     loadGame()
     assert old_arr == arr
 
+def test_pregrada():
+    pregrada1=0
+    pregrada=arr.count('#')
+    saveGame(k)
+    file = open("text.txt", "r")
+    if '#' in file:
+        pregrada1+=1
+    assert pregrada==pregrada1
 
 
